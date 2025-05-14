abda7cf71482b54032f5f773f2556a1f815116a4
Najehsaidani <151434223+Najehsaidani@users.noreply.github.com>
2025-05-14 17:38:22 +0100

    g

--- backend/club-service/src/main/java/com/iset/clubservice/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.iset.clubservice.repository;
-
-import com.iset.clubservice.model.entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
